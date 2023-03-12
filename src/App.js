c2748dc67d48711d86a49d23b083dcc8b6637a75
CaiqueJar <caiquedm11@gamil.com>
2023-03-12 20:51:04 -0300

    many things

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/esm/Button';
+import Table from 'react-bootstrap/esm/Table';
+import Form from 'react-bootstrap/esm/Form';
 import api from './api';
+import  './App.css';
+import CreateModal from './components/CreateModal';
+import { BiTrash } from "react-icons/bi";
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+
 
 class App extends Component {
 
@@ -12,24 +21,66 @@ class App extends Component {
 
     this.setState({ tasks: response.data });
   }
+  
+  
 
   render() {
 
     const { tasks } = this.state;
 
     return(
-      <div>
-        <h1>List tasks</h1>
-        {console.log(tasks)}
-        {tasks.map(task => (
-          <li key={task.id}>
-            <h2>{task.name}</h2>
-          </li>
-        ))}
+      <div className="center">
+        <div>
+        <h1>Todo List App</h1>
 
+        </div>
+        <hr />
+        <div className="tasks">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Task Name</th>
+                <th>Task date</th>
+                <th>Task status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map(task => (
+                <tr>
+                  <td>{task.id}</td>
+                  <td>{task.name}</td>
+                  <td>{task.hour}</td>
+                  <td>
+                    <Row>
+                      <Col>
+                        <Form.Check 
+                            type="checkbox"
+                            id="check"
+                            label="Check"
+                            checked={() => console.log("asdasd")}
+                          />
+                      </Col>
+                      <Col>
+                        <Button variant='danger' onClick={() => api.delete('http://localhost:8080/api/v1/task/'+task.id).then(window.location.reload())}><BiTrash /></Button>
+                      </Col>
+                    </Row>
+                    </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+
+        <hr />
+
+        <div className='buttons'>
+          <CreateModal />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/CreateModal.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import api from '../api';
+
+function CreateModal() {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState();
+  const [date, setDate] = useState();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleName = (e) => setName(e.target.value);
+  const handleDate = (e) => setDate(e.target.value);
+
+  const handleSubmit=(e)=> {
+    e.preventDefault()
+
+    const data = {
+      name: name,
+      hour: date,
+      task_status: 1
+    }
+
+    api.post('http://localhost:8080/api/v1/task', data);
+    window.location.reload(false);
+
+  }
+
+  return (
+    <>
+      <Button variant="success" onClick={handleShow}>
+        New task
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a new task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <Row>
+                <Col>
+                  <Form.Control placeholder="Task name" onChange={handleName} />
+                </Col>
+                <Col>
+                  <Form.Control type='date' placeholder="Task date" onChange={handleDate}/>
+                </Col>
+              </Row>
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="success" type='submit'>
+              Add
+            </Button>
+          </Modal.Footer>
+        </Form>
+
+      </Modal>
+    </>
+  );
+}
+
+export default CreateModal;
